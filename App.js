95bd89ab2424b4b7c6482cabe1abf547a5a3c329
Roger <rdpender3@gmail.com>
2019-04-04 22:17:44 -0700

    took out some extra stuff

--- App.js
+++ App.js
@@ -1,15 +1,7 @@
 import React, { Component } from "react";
-import { Platform, StyleSheet } from "react-native";
 import { createAppContainer, createStackNavigator } from "react-navigation";
 import HomeScreen from "./HomeScreen";
 import QuoteScreen from "./QuoteScreen";
-
-const instructions = Platform.select({
-  ios: "Press Cmd+R to reload,\n" + "Cmd+D or shake for dev menu",
-  android:
-    "Double tap R on your keyboard to reload,\n" +
-    "Shake or press menu button for dev menu"
-});
 
 export default class App extends Component {
   render() {
@@ -28,31 +20,3 @@ const AppNavigator = createStackNavigator(
 );
 
 const AppContainer = createAppContainer(AppNavigator);
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    backgroundColor: "#efefef"
-  },
-  readyText: {
-    fontSize: 20,
-    fontStyle: "italic",
-    color: "#fff"
-  },
-  button: {
-    backgroundColor: "#859a9b",
-    borderRadius: 20,
-    padding: 10,
-    marginBottom: 20,
-    shadowColor: "#303838",
-    shadowOffset: { width: 0, height: 5 },
-    shadowRadius: 10,
-    shadowOpacity: 0.35
-  },
-  buttonImage: {
-    width: 200,
-    height: 200
-  }
-});
